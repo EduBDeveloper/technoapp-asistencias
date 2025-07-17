24aed2e128aa9c943c03fa78bf1ac1567031bffd
Eduardo Balbuena <127214755+EduBDeveloper@users.noreply.github.com>
2025-07-17 20:06:23 -0300

    Delete backend directory

--- backend/src/main/java/com/coditech/gestion/repository/TipoEquipoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coditech.gestion.repository;
-
-import com.coditech.gestion.entities.TipoEquipo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TipoEquipoRepository extends JpaRepository<TipoEquipo, Long> {
-}
